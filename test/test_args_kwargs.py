8c82406267576ef601bfacc364830ad83ffa9cd1
phoenix fate <sm516116978@outlook.com>
2020-07-06 09:42:57 +0800

    基础复习

--- test/test_args_kwargs.py
+++ test/test_args_kwargs.py
@@ -1,3 +1,6 @@
+import string
+
+
 def test(*args):
     print(type(args), args)
 
@@ -6,3 +9,10 @@ test()
 test(1)
 test(1, 2)
 test(1, 2, 3, 'a', [1, 2, 3])
+a: int = 10
+print(a)
+name: string = "abc"
+print(name)
+print(type(name))
+name2 = "bcd"
+print(type(name2))
